1612e778bbc08c58fbf5ffd5cc970f6b7a3245ad
matt <matthieugarban@gmail.com>
2023-07-15 07:43:01 +0200

    setup pagination

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,18 +3,39 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $ingRepo): Response
+    /**
+     * This function displays all ingredients
+     *
+     * @param IngredientRepository $ingRepo
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'app_ingredient', methods:['GET'])]
+    public function index(IngredientRepository $ingRepo, Request $request, PaginatorInterface $paginator): Response
     {
-        $ingredients = $ingRepo->findAll();
+        // $ingredients = $ingRepo->findAll();
+
+        $ingredients = $paginator->paginate(
+            $ingRepo->findAll(), // $query, query NOT result
+            $request->query->getInt('page', 1), // page number
+            10 // limit per page
+        );
+
+        $totalIngredients = $ingredients->getTotalItemCount();
+
+
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' =>$ingredients
+            'ingredients' => $ingredients,
+            'totalIng' => $totalIngredients
         ]);
     }
 }
