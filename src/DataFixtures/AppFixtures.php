80aaa9eeaad8475b0c12ea091445dd17b564116d
matt <matthieugarban@gmail.com>
2023-08-21 23:26:13 +0200

    using EntityListeners

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,25 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Recette;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AppFixtures extends Fixture
 {
+    private $faker;
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->faker = Factory::create('fr_FR');
+        $this->hasher = $hasher;
+    }
     public function load(ObjectManager $manager): void
     {
             $faker = Factory::create('fr_FR');
@@ -42,10 +54,24 @@ class AppFixtures extends Fixture
                 }
             
                 $manager->persist($recette);
-
             }
+            
+            //Users
+            for($i = 0; $i<10; $i++){
+                $user = new User();
+                $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0,1)===1? $this->faker->firstName():null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
 
-
+                // $hashPassword = $this->hasher->hashPassword($user,'password');
+                // $user->setPassword($hashPassword);
+                
+                $manager->persist($user);
+            }
+            
         $manager->flush($ingredient);
     }
+    
 }
