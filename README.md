1f58fa89c4c50ed1b1c86ca376c19461f565d137
matt <matthieugarban@gmail.com>
2023-09-13 14:25:40 +0200

    adding readMe & RegistrationType

---
+++ README.md
@@ -0,0 +1,6 @@
+à partir de 4:22:20
+
+création d'un formulaire d'inscription
+php bin/console make:form
+on donne le nom du fichier qui contient le formulaire
+et la classe à laquelle il doit être rattaché
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fullName', TextType::class, [
+                'attr' =>[
+                    'class' => 'form-control',
+                    'minlength' =>'2',
+                    'maxlength' => '50',
+                ]
+            ])
+            ->add('pseudo')
+            ->add('email')
+            ->add('password')
+            ->add('sunmit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
